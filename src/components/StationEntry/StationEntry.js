5e08e0bd434f05c7b2773cc8a45f3c9a2bfddbd9
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-10-25 19:19:20 +0300

    Installed axios, Connected with the api

--- src/components/StationEntry/StationEntry.js
+++ src/components/StationEntry/StationEntry.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./StationEntry.module.css";
 const StationEntry = props => {
+  console.log(props.station);
   return (
-    <div>
-      <div className={styles.address}>{props.address}</div>
-      <div></div>
+    <div className={styles.main}>
+      <div className={styles.address}>{props.station.address}</div>
+      <div className={styles.speedAndType}>
+        <div className={styles.type}>
+          {props.station.speed.min === props.station.speed.max
+            ? props.station.speed.max
+            : `${props.station.speed.min} - ${props.station.speed.max}`}
+        </div>
+        <div className={styles.speed}></div>
+      </div>
     </div>
   );
 };
--- src/containers/StationsListing/StationsListing.js
+++ src/containers/StationsListing/StationsListing.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import styles from "./StationsListing.module.css";
+import StationEntry from "../../components/StationEntry/StationEntry";
+const axios = require("axios");
 class StationsListing extends Component {
+  state = { stations: [] };
+  async componentDidMount() {
+    const url = "http://18.216.165.155:3000/v1/stations/all";
+    const data = await axios.get(url);
+
+    this.setState((prevState, props) => ({
+      stations: data.data.stations
+    }));
+  }
+
   render() {
+    const stationEntries = this.state.stations.map(station => (
+      <StationEntry station={station} key={station.address} />
+    ));
     return (
       <div className={styles.main}>
         <div className={styles.title}>ELECTROMESH</div>
@@ -13,7 +28,7 @@ class StationsListing extends Component {
           className={styles.searchBar}
           placeholder="type in city or address of the station"
         ></input>
-        <div className={styles.stationsListings}></div>
+        <div className={styles.stationsListings}>{stationEntries}</div>
       </div>
     );
   }
